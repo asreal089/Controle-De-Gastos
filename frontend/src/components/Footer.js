d513e36de9cf5302863a451a988fb09ec2133958
asreal089 <lexx_89@hotmail.com>
2020-07-21 19:04:48 -0300

    add formulario + ajustes pequenos

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Footer extends Component {
 	render() {
 		return (
-			<footer class="page-footer">
-				<div class="footer-copyright">
-					<div class="container">
-						<p class="grey-text text-lighten-4">
+			<footer className="page-footer">
+				<div className="footer-copyright">
+					<div className="container">
+						<p className="grey-text text-lighten-4">
 							“A chave para a vitória é criar as rotinas certas.”
 							-<strong>Charles Duhigg</strong>
 						</p>
@@ -14,7 +14,7 @@ class Footer extends Component {
 
 					<p>Entre em contato: </p>
 					<a
-						class="grey-text text-lighten-4 right"
+						className="grey-text text-lighten-4 right"
 						href="mailto:contato@controle.de.gasto.com.br"
 					>
 						contato@controle.de.gasto.com.br
--- frontend/src/components/NovaReceita.js
+++ frontend/src/components/NovaReceita.js
@@ -3,16 +3,93 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class NovaReceita extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			nome: '',
+			descricao: '',
+			valor: null,
+			isMensal: false,
+		};
+	}
+
+	mySubmitHandler = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+		alert('olar');
+	};
+	myChangeHandler = (event) => {
+		console.log(event.target.name);
+		console.log(event.target.value);
+		let nam = event.target.name;
+		let val = event.target.value;
+		this.setState({ [nam]: val });
+	};
+
+	toggleChange = (event) => {
+		this.setState({
+			isMensal: !this.state.isMensal,
+		});
+	};
+
 	componentDidMount() {
 		if (!this.props.auth) {
 			this.props.history.push(`/`);
 		}
 	}
+
 	render() {
-		return <h1>olar nova receita</h1>;
+		return (
+			<div className="defaultForm">
+				<form onSubmit={this.mySubmitHandler}>
+					<input
+						placeholder="Nome"
+						type="text"
+						value={this.state.nome}
+						name="nome"
+						onChange={this.myChangeHandler}
+					/>
+					<input
+						type="text"
+						name="descricao"
+						placeholder="Descrição"
+						value={this.state.descricao}
+						onChange={this.myChangeHandler}
+					/>
+					<input
+						name="valor"
+						type="number"
+						placeholder="Valor"
+						value={this.state.valor}
+						onChange={this.myChangeHandler}
+					/>
+					<div className="switch">
+						É mensal:
+						<br />
+						<div className="switch">
+							<label>
+								Não
+								<input
+									name="isMensal"
+									type="checkbox"
+									onChange={this.toggleChange}
+								/>
+								<span className="lever"></span>
+								Sim
+							</label>
+						</div>
+					</div>
+					<button className="button" type="submit">
+						Cadastrar
+					</button>
+				</form>
+			</div>
+		);
 	}
 }
+
 function mapStateToProps({ auth }) {
 	return { auth };
 }
+
 export default connect(mapStateToProps, actions)(NovaReceita);
