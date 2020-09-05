c6c934867ed62e4924fe60a70581c068da56b509
asreal089 <lexx_89@hotmail.com>
2020-09-05 12:23:43 -0300

    Termino de refctor, agora usando state para que delete funcione.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,62 +3,20 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { Link } from 'react-router-dom';
 import TableRegister from './TableRegister';
-const axios = require('axios');
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			registros: [],
-			renda: [],
-			totalTipo: [],
-		};
-	}
-
 	componentDidMount() {
 		if (!this.props.auth) {
 			this.props.history.push(`/`);
 		}
-
-		axios.get('api/gastos').then((res) => {
-			const registros = res.data;
-			this.setState({ registros });
-		});
-
-		axios.get('api/renda').then((res) => {
-			const renda = res.data;
-			this.setState({ renda });
-		});
 	}
-
-	handleDeleteRegistro = async (reg) => {
-		await axios({
-			method: 'DELETE',
-			url: 'api/gastos',
-			data: reg,
-		}).then((res) => {
-			axios.get('api/gastos').then((res) => {
-				const registros = res.data;
-				this.setState({ registros });
-			});
-		});
-	};
 
 	render() {
 		return (
 			<div className="registros_container">
-				{this.state.registros && (
-					<TableRegister
-						name="Gastos"
-						registros={this.state.registros}
-					/>
-				)}
-				{this.state.renda && (
-					<TableRegister
-						name="Fontes de Renda"
-						registros={this.state.renda}
-					/>
-				)}
+				<TableRegister tipo_registro="Gastos" />
+				<TableRegister tipo_registro="Fontes de Renda" />
+
 				<Link className="button" to="/registro">
 					Cadastrar Novo Registro
 				</Link>
--- frontend/src/components/TableRegister.js
+++ frontend/src/components/TableRegister.js
@@ -4,8 +4,50 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { FiTrash2, FiEdit } from 'react-icons/fi';
 const moment = require('moment');
+const axios = require('axios');
 
 class TableRegister extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			registros: [],
+		};
+	}
+
+	componentDidMount() {
+		if (this.props.tipo_registro === 'Gastos') {
+			axios.get('api/gastos').then((res) => {
+				const registros = res.data;
+				this.setState({ registros });
+			});
+		} else {
+			axios.get('api/renda').then((res) => {
+				const registros = res.data;
+				this.setState({ registros });
+			});
+		}
+	}
+
+	handleDeleteRegistro = async (reg) => {
+		await axios({
+			method: 'DELETE',
+			url: 'api/gastos',
+			data: reg,
+		}).then((res) => {
+			if (this.props.tipo_registro === 'Gastos') {
+				axios.get('api/gastos').then((res) => {
+					const registros = res.data;
+					this.setState({ registros });
+				});
+			} else {
+				axios.get('api/renda').then((res) => {
+					const registros = res.data;
+					this.setState({ registros });
+				});
+			}
+		});
+	};
+
 	render() {
 		return (
 			<div>
@@ -22,7 +64,7 @@ class TableRegister extends Component {
 						</tr>
 					</thead>
 					<tbody>
-						{this.props.registros.map((registro) => (
+						{this.state.registros.map((registro) => (
 							<tr key={registro._id}>
 								<td>{registro.tipo}</td>
 								<td>{registro.descricao}</td>
